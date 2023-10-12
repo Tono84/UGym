51500a6b3ff7459fd8955ec73a706d36d18eea7a
Dennis Serrano <105755769+Dennis-SB@users.noreply.github.com>
2023-10-11 18:33:06 -0600

    Delete UGym_NetCore directory

--- UGym_NetCore/UGym/BackEnd/Models/QRCodeModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BackEnd.Models
-{
-    public class QRCodeModel
-    {
-        public int QrcodeId { get; set; }
-        public string LinkQr { get; set; } = null!;
-    }
-}
--- UGym_NetCore/UGym/FrontEnd/Models/QRCodeViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FrontEnd.Models
-{
-    public class QRCodeViewModel
-    {
-        public int QrcodeId { get; set; }
-        public string LinkQr { get; set; } = null!;
-    }
-}
