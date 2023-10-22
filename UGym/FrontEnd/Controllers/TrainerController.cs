67e8b8804da7c679ab085db009a6b8703756c743
José Antonio Solórzano Flores <98479861+Tono84@users.noreply.github.com>
2023-10-22 12:35:24 -0600

    vistas entrenador

---
+++ UGym/FrontEnd/Controllers/TrainerController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FrontEnd.Controllers
+{
+    public class TrainerController : Controller
+    {
+        // GET: Trainer
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Profile()
+        {
+            return View();
+        }
+
+        public ActionResult Clients()
+        {
+            return View();
+
+        }
+
+        public ActionResult Routines()
+        {
+            return View();
+        }
+
+        public ActionResult Evaluations()
+        {
+            return View();
+        }
+
+        public ActionResult Drafts()
+        {
+            return View();
+        }
+
+        public ActionResult Gestion()
+        {
+            return View();
+        }
+
+        public ActionResult Classes()
+        {
+            return View();
+        }
+
+        public ActionResult ModifyClass()
+        {
+            return View();
+        }
+    }
+}
