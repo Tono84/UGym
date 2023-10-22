af3bbcbfaeb6abd5ca163c4968b4327d7ecd58ce
José Antonio Solórzano Flores <98479861+Tono84@users.noreply.github.com>
2023-10-22 12:28:50 -0600

    Vistas de terapista

---
+++ UGym/FrontEnd/Controllers/TherapistController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FrontEnd.Controllers
+{
+    public class TherapistController : Controller
+    {
+        // GET: Therapist
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Profile()
+        {
+            return View();
+        }
+
+        public ActionResult Clients()
+        {
+            return View();
+        }
+
+        public ActionResult Reports()
+        {
+            return View();
+        }
+    }
+}
